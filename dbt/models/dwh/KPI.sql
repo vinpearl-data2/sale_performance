16edab37bfdd6fba35ef6a25dd231ed4465cc5d0
liendtm <v.liendtm@vinpearl.com>
2024-05-20 12:26:08 +0700

    KPI

---
+++ dbt/models/dwh/KPI.sql
@@ -0,0 +1,7 @@
+select *, case when propertycode like '%Nha Trang%' then "Region 1" 
+               when propertycode like '%Đà Nẵng%' or propertycode like '%Hội An%' or propertycode like '%Hạ Long%' then 'Region 2'
+               else "Region 3"
+               end as Custom
+
+
+from `vp-dwh-prod-c827.VINPEARL_TRAVEL.VIN3S_DM_VP_MONTHLY_KPI`
